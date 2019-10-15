35ec75a8dd8f99d99e19f064ed79259fc5e9763c
vule <vule840@gmail.com>
2019-10-15 10:31:39 +0200

    Prvi

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,20 +1,37 @@
 <template>
-
-  <div class="footer-container">
+    <div class="footer-container">
         <footer class="wrapper flex justify-between container mx-auto pb-8">
             <ul class="text-left">
-                <li><a href="#">Naslovnica</a></li>
-                <li><a href="/kako-sudjelovati.html">Kako sudjelovati</a></li>
-                <li><a href="#">Glasaj za svog favorita</a></li>
-                <li><a href="/good-cause.html">Good cause glasanje</a></li>
-                <li><a href="#">Raspored crtanja</a></li>
-                <li><a href="#">Nagrade</a></li>
-                <li><a href="#">Pravila</a></li>
-                <li><a href="#">Objava dobitnika </a></li>
+                <li>
+                    <router-link to="/"><span>Naslovnica</span></router-link>
+                </li>
+                <li>
+                    <router-link to="/kako-sudjelovati"><span>Kako sudjelovati</span></router-link>
+                </li>
+                <li>
+                    <router-link to="/glasaj-za-svog-favorita"><span>Glasaj za svog favorita</span></router-link>
+                </li>
+                <li>
+                    <router-link to="/good-cause"><span>Good cause glasanje</span></router-link>
+                </li>
+                <li>
+                    <router-link to="/raspored-crtanja"><span>Raspored crtanja</span></router-link>
+                </li>
+                <li>
+                    <router-link to="/nagrade"><span>Nagrade</span></router-link>
+                </li>    
+                <li>
+                    <router-link to="/pravila"><span>Pravila</span></router-link>
+                </li>
+                <li>
+                    <router-link to="/objava-dobitnika"><span>Objava dobitnika </span></router-link>
+                </li>
+
+               
             </ul>
             <div class="flex self-end text-white mr-6 doodle">
-                    <img src="@/assets/doodle.svg">
-                </div>
+                <img src="@/assets/doodle.svg">
+            </div>
         </footer>
         <div class="cookies">
             <div class="container  mx-auto flex justify-between py-4">
@@ -32,59 +49,61 @@
                 <div class="flex   text-white mr-6 doodle">
                     1
                 </div>
-                
             </div>
         </div>
     </div>
-  
 </template>
-
 <script>
 export default {
-  name: 'Footer',
-  props: {
-    msg: String
-  }
+    name: 'Footer',
+    props: {
+        msg: String
+    }
 }
 </script>
-
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-    // FOOTER
-.footer-container{
+// FOOTER
+.footer-container {
     // position: fixed;
- //    bottom: 0px;
- color:black;
- padding-top: 3rem;
+    //    bottom: 0px;
+    color: black;
+    padding-top: 3rem;
     width: 100%;
-    
-    ul{
-        a{
-            text-decoration:underline;
-            &:hover{
+
+    ul {
+        a {
+            text-decoration: underline;
+
+            &:hover {
                 font-weight: bold;
             }
-            &:active{
+
+            &:active {
                 font-weight: bold;
             }
         }
     }
-    .cookies{
+
+    .cookies {
         background: white;
-        a{
-            text-decoration:underline;
+
+        a {
+            text-decoration: underline;
         }
     }
-    .ikea-logo{
+
+    .ikea-logo {
         position: relative;
-            top: -44px;
+        top: -44px;
     }
+
     // .doodle{
     //  position: relative;
- //     top: -113px;
+    //     top: -113px;
     // }
-    
 
-    
+
+
 }
 </style>
--- src/views/Nagrade.vue
+++ src/views/Nagrade.vue
@@ -1,23 +1,24 @@
 <template>
-    <div id="home">
+    <div id="nagrade">
         <Header />
          <div class="container mx-auto">
         <!-- Lets play-->
         <div class="flex mb-4 pb-8 pt-16">
             <div class="w-1/2 mx-auto text-center">
-                <h2 class="naslov">Let’s play!</h2>
-                <p>Raise awareness of the importance of play for all – for both children and adults – for them to play more.</p>
+                <h2 class="naslov">Nagrade</h2>
+                <p>Svim učesnicima takmičenja biće uručena diploma.</p>
+                <p>Svaki od 15 finalista će primiti nagradu tokom proslave/PR događaja u robnoj kući. Svih 6 međunarodnih pobednika će dobiti svoj primerak plišane igračke izrađene prema crtežu koji su napravili.</p>
             </div>
         </div>
         <!-- Gumbi -->
-        <div class="flex">
+      <!--   <div class="flex">
             <div class="w-1/2 mx-auto text-center">
                 <button class="zuti_gumb py-4 px-4 rounded ">
                     Glasaj za svog favorita
                 </button><br>
                 <button class="plavi_gumb py-4 px-4 rounded ">Kako sudjelovati?</button>
             </div>
-        </div>
+        </div> -->
     </div>
         <Footer />
     </div>
@@ -29,7 +30,7 @@ import Footer from '@/components/Footer.vue'
 // import HomeContent from '@/components/HomeContent.vue'
 
 export default {
-    name: 'home',
+    name: 'nagrade',
     components: {
         Header,
         Footer,
